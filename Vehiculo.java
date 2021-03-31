81a7207b8f31156d1bf93cbc6bbe570e2a09f258
Viviana Hoyos <vhoyoss@eafit.edu.co>
2021-03-31 16:58:53 -0500

    hola

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,13 +1,13 @@
-
 public class Vehiculo
 {
   public static Vehiculo[][]vehiculos; 
   public static int tamaño;
-  public static int cantidad;
+  public static int cantidad = 0;
   private String placa;
   private String marca;
   private String color;
   private int valorComercial;
+<<<<<<< HEAD
   public void setPlaca(String placa){
       this.placa= placa;
     }
@@ -22,3 +22,17 @@ public class Vehiculo
     } 
     
 }
+=======
+  public Vehiculo(){  }
+  public Vehiculo(String p, String m, String c, int v){
+   this.placa = p;
+   this.marca = m;
+   this.color = c;
+   this.valorComercial = v;
+  }
+  public Vehiculo(String p, String m, String c){
+   this(p,m,c,30000000);
+  }
+  
+}
+>>>>>>> 7d1a54fcbbcbe70baa64f1b5ef64aa6b5121c7a0
