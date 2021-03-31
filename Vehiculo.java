7d1a54fcbbcbe70baa64f1b5ef64aa6b5121c7a0
Pablo Micolta <pablomicoltal@gmail.com>
2021-03-31 16:47:01 -0600

    Constructores

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,10 +1,21 @@
-
 public class Vehiculo
 {
   public static Vehiculo[][]vehiculos; 
   public static int tamaño;
-  public static int cantidad;
+  public static int cantidad = 0;
   private String placa;
   private String marca;
+  private String color;
+  private int valorComercial;
+  public Vehiculo(){  }
+  public Vehiculo(String p, String m, String c, int v){
+   this.placa = p;
+   this.marca = m;
+   this.color = c;
+   this.valorComercial = v;
+  }
+  public Vehiculo(String p, String m, String c){
+   this(p,m,c,30000000);
+  }
   
 }
