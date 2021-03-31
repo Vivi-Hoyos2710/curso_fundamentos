7c87a53c54039eaa764389a61d17c177c19411b4
Viviana Hoyos <vhoyoss@eafit.edu.co>
2021-03-31 16:57:16 -0500

    Modificado g

--- Vehiculo.java
+++ Vehiculo.java
@@ -6,5 +6,19 @@ public class Vehiculo
   public static int cantidad;
   private String placa;
   private String marca;
-  
+  private String color;
+  private int valorComercial;
+  public void setPlaca(String placa){
+      this.placa= placa;
+    }
+    public void setMarca(String marca){
+      this.marca= marca;
+    }
+   public void setColor(String color){
+      this.color= color;
+    }  
+   public void setValorComercial(int valor){
+      this.valorComercial= valor;
+    } 
+    
 }
