74bb0277b5555b6181257d6236f12967e4f6db7e
Vivi-Hoyos2710 <vhoyoss@eafit.edu.co>
2021-04-02 16:12:48 -0500

    correcciones vehiculo

--- Principal.java
+++ Principal.java
@@ -1,33 +1,10 @@
+import java.util.Scanner;
 
-/**
- * Write a description of class Principal here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Principal
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Principal
-     */
-    public Principal()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int cantidadPisos, cantidadEspacios;
+        //solicitando cantidad de pisos del edificio y cantidad de espacios
+        
     }
 }
--- Vehiculo.java
+++ Vehiculo.java
@@ -1,63 +1,77 @@
-public class Vehiculo
-{
-  public static Vehiculo[][]vehiculos; 
+public class Vehiculo {
+  public static Vehiculo[][] vehiculos;
   public static int tamaño;
   public static int cantidad = 0;
   private String placa;
   private String marca;
   private String color;
   private int valorComercial;
-  public Vehiculo(){  }
-  public Vehiculo(String p, String m, String c, int v){
-   this.placa = p;
-   this.marca = m;
-   this.color = c;
-   this.valorComercial = v;
-   cantidad++;
+
+  public Vehiculo() {
   }
-  public Vehiculo(String p, String m, String c){
-   this(p,m,c,30000000);
+
+  public Vehiculo(String p, String m, String c, int v) {
+    this.placa = p;
+    this.marca = m;
+    this.color = c;
+    this.valorComercial = v;
+    cantidad++;
   }
-    public void setPlaca(String placa){
-      this.placa= placa;
-    }
-    public void setMarca(String marca){
-      this.marca= marca;
-    }
-   public void setColor(String color){
-      this.color= color;
-    }  
-   public void setValorComercial(int valor){
-      this.valorComercial= valor;
-    } 
-    public String getPlaca(){
-      return this.placa;
-    }
-  public String getMarca(){
-      return this.marca;
-    }
-  public String getColor(){
-      return this.color;
-    }
-  public int getValorComercial(){
-      return this.valorComercial;
-    }
+
+  public Vehiculo(String p, String m, String c) {
+    this(p, m, c, 30000000);
+  }
+
+  public void setPlaca(String placa) {
+    this.placa = placa;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public void setValorComercial(int valor) {
+    this.valorComercial = valor;
+  }
+
+  public String getPlaca() {
+    return this.placa;
+  }
+
+  public String getMarca() {
+    return this.marca;
+  }
+
+  public String getColor() {
+    return this.color;
+  }
+
+  public int getValorComercial() {
+    return this.valorComercial;
+  }
+
   public String toString(){
-    String info = "Marca: "+this.marca+" Placa: "+this.placa+" Color: "+" Precio: "this.valorComercial;
+    String info = "Marca: "+this.marca+" Placa: "+this.placa+" Color: "+" Precio: "+this.valorComercial;
     return info;
   }
-    public String toStringVehiculo(){
+
+  public String toStringVehiculos(){
       String info_de_todos = "";
       for(int i=0;i < vehiculos.length;i++){
           for(int j=0;j<vehiculos[i].length;j++){
              if(vehiculos[i][j]!= null) {
-                 info_de_todos += vehiculos[i][j].toString()"\n";
+                 info_de_todos += vehiculos[i][j].toString()+"\n";
              }
           }
       }
+      return info_de_todos;
   }
-    public static int cantidadVehiculos(){
-      return cantidad;
-    }
-}
 
+  public static int cantidadVehiculos() {
+    return cantidad;
+  }
+}
