fc01bfddcf04a6203554c9cb0bda220cf0018c6f
Vivi-Hoyos2710 <vhoyoss@eafit.edu.co>
2021-04-30 21:14:53 -0500

    arreglo cond en caso 3

--- Principal.java
+++ Principal.java
@@ -12,9 +12,9 @@ public class Principal {
         cantidadEspacios = entrada.nextInt();
         Vehiculo.vehiculos = new Vehiculo[cantidadPisos][cantidadEspacios];
         Sensor.sensores = new Sensor[cantidadPisos][cantidadEspacios];
-        for(int i=0; i<Sensor.sensores.length;i++){
-            for(int j=0; j<Sensor.sensores[i].length;j++){
-                Sensor.sensores[i][j]=new Sensor(0);
+        for (int i = 0; i < Sensor.sensores.length; i++) {
+            for (int j = 0; j < Sensor.sensores[i].length; j++) {
+                Sensor.sensores[i][j] = new Sensor(0);
             }
         }
         Vehiculo.tamano = cantidadEspacios * cantidadPisos;
@@ -34,100 +34,107 @@ public class Principal {
         while (seleccion != 0) {
 
             switch (seleccion) {
-            case 1:
-                System.out.println(Sensor.sensorLibre());
-                break;
-            case 2:
-                System.out.println("Ingrese en que piso y espacio desea parquearse: ");
-                pisoDeseado = entrada.nextInt()-1;
-                lugarDeseado = entrada.nextInt()-1;
-                // Primero, verificar que la cantidad de vehiculos actual sea menor a el tamaño permitido de vehiculos totales.
-                //probando esto.
-                if((0<=pisoDeseado) && (0<=lugarDeseado) && (pisoDeseado<cantidadPisos) && (lugarDeseado<cantidadEspacios)){
-                    if(Vehiculo.cantidad<Vehiculo.tamano){
-                        if(Sensor.sensores[pisoDeseado][lugarDeseado].getEstado()==0){
-                        System.out.println("Por favor ingresar placa,marca y color separados por espacios");
-                        String placa= entrada.next();
-                        String marca= entrada.next();
-                        String color= entrada.next();
-                        Vehiculo.vehiculos[pisoDeseado][lugarDeseado]= new Vehiculo(placa,marca,color);
-                        Sensor.sensores[pisoDeseado][lugarDeseado].setEstado(1);
-                        System.out.println(Vehiculo.vehiculos[pisoDeseado][lugarDeseado]);
-                    }
-                     else{
-                        System.out.println("Esta ocupado");
-                    }
-                }
-                       
-                else{
-                    System.out.println("Esta lleno");
-                }
-            }
-            else{
-                System.out.println("Este espacio no existe");
-            }
-          
-    
-                break;
-            case 3:
-                System.out.println("Ingrese en que piso y espacio desea parquearse: ");
-                pisoDeseado = entrada.nextInt()-1;
-                lugarDeseado = entrada.nextInt()-1;
-                if ((0<=pisoDeseado) && (0<=lugarDeseado) && (pisoDeseado<cantidadPisos) && (lugarDeseado<cantidadEspacios)) {
-                    if (Sensor.sensores[pisoDeseado][lugarDeseado].getEstado() == 0) {
-                        System.out.println("Ingrese la placa,marca y color separado por un espacio");
-                        String placa = entrada.next();
-                        String marca = entrada.next();
-                        String color = entrada.next();
-                        System.out.println("Ingrese el valor comercial del vehiculo: ");
-                        int valor = entrada.nextInt();
-                        Vehiculo.vehiculos[pisoDeseado][lugarDeseado] = new Vehiculo(placa, marca, color, valor);
-                        Sensor.sensores[pisoDeseado][lugarDeseado].setEstado(1);
-                        System.out.println(Vehiculo.vehiculos[pisoDeseado][lugarDeseado]);
+                case 1:
+                    System.out.println(Sensor.sensorLibre());
+                    break;
+                case 2:
+                    System.out.println("Ingrese en que piso y espacio desea parquearse: ");
+                    pisoDeseado = entrada.nextInt() - 1;
+                    lugarDeseado = entrada.nextInt() - 1;
+                    // Primero, verificar que la cantidad de vehiculos actual sea menor a el tamaño
+                    // permitido de vehiculos totales.
+                    // probando esto.
+                    if ((0 <= pisoDeseado) && (0 <= lugarDeseado) && (pisoDeseado < cantidadPisos)
+                            && (lugarDeseado < cantidadEspacios)) {
+                        if (Vehiculo.cantidad < Vehiculo.tamano) {
+                            if (Sensor.sensores[pisoDeseado][lugarDeseado].getEstado() == 0) {
+                                System.out.println("Por favor ingresar placa,marca y color separados por espacios");
+                                String placa = entrada.next();
+                                String marca = entrada.next();
+                                String color = entrada.next();
+                                Vehiculo.vehiculos[pisoDeseado][lugarDeseado] = new Vehiculo(placa, marca, color);
+                                Sensor.sensores[pisoDeseado][lugarDeseado].setEstado(1);
+                                System.out.println(Vehiculo.vehiculos[pisoDeseado][lugarDeseado]);
+                            } else {
+                                System.out.println("Esta ocupado");
+                            }
+                        }
 
+                        else {
+                            System.out.println("Esta lleno");
+                        }
                     } else {
-                        System.out.println("Esta ocupado mijo");
+                        System.out.println("Este espacio no existe");
                     }
-                } else {
-                    System.out.println("Este lugar no existe");
-                }
-                break;
-            case 4:
-                System.out.println(Vehiculo.toStringVehiculos());
-                break;
-            case 5:
-                System.out.println("Hay "+Vehiculo.cantidad+" vehiculos parqueados");
-                break;
-            case 6:
-                System.out.println("Ingrese en que piso y espacio desea parquearse: ");
-                pisoDeseado = entrada.nextInt()-1;
-                lugarDeseado = entrada.nextInt()-1;
-                if ((0<=pisoDeseado) && (0<=lugarDeseado) && (pisoDeseado<cantidadPisos) && (lugarDeseado<cantidadEspacios)) {
-                if (Sensor.sensores[pisoDeseado][lugarDeseado] != null) {
-                    System.out.println(Sensor.sensores[pisoDeseado][lugarDeseado].toString());
-                } 
-            }
-            else {
-                    System.out.println("Este lugar no existe");
-                }
-                
-                break;
-            case 7:
-                System.out.println(Sensor.sensoresEstado());
-                break;
-            case 8:
-            System.out.println("Ingrese el color deseado");
-            String color_deseado= entrada.next();
-                System.out.println(Vehiculo.filtroColor(color_deseado));
-                break; 
-            case 9:
-            for(int i=0;i<Vehiculo.Ordenar().length;i++){
-              System.out.println(Vehiculo.Ordenar()[i].toString());        
-            }
-            break;
-            default:
-                System.out.println("Comando incorrecto");
-                break;
+
+                    break;
+                case 3:
+                    System.out.println("Ingrese en que piso y espacio desea parquearse: ");
+                    pisoDeseado = entrada.nextInt() - 1;
+                    lugarDeseado = entrada.nextInt() - 1;
+                    if ((0 <= pisoDeseado) && (0 <= lugarDeseado) && (pisoDeseado < cantidadPisos)
+                            && (lugarDeseado < cantidadEspacios)) {
+                        if (Vehiculo.cantidad < Vehiculo.tamano) {
+                            if (Sensor.sensores[pisoDeseado][lugarDeseado].getEstado() == 0) {
+                                System.out.println("Ingrese la placa,marca y color separado por un espacio");
+                                String placa = entrada.next();
+                                String marca = entrada.next();
+                                String color = entrada.next();
+                                System.out.println("Ingrese el valor comercial del vehiculo: ");
+                                int valor = entrada.nextInt();
+                                Vehiculo.vehiculos[pisoDeseado][lugarDeseado] = new Vehiculo(placa, marca, color,
+                                        valor);
+                                Sensor.sensores[pisoDeseado][lugarDeseado].setEstado(1);
+                                System.out.println(Vehiculo.vehiculos[pisoDeseado][lugarDeseado].toString());
+
+                            }
+                            else{
+                                System.out.println("Esta ocupado mijo");
+                            }
+                        }
+                        else{
+                            System.out.println("Esta lleno");
+                        } 
+                    } else{
+                        System.out.println("Este lugar no existe");
+                    }
+                    break;
+                case 4:
+                    System.out.println(Vehiculo.toStringVehiculos());
+                    break;
+                case 5:
+                    System.out.println("Hay " + Vehiculo.cantidad + " vehiculos parqueados");
+                    break;
+                case 6:
+                    System.out.println("Ingrese en que piso y espacio desea parquearse: ");
+                    pisoDeseado = entrada.nextInt() - 1;
+                    lugarDeseado = entrada.nextInt() - 1;
+                    if ((0 <= pisoDeseado) && (0 <= lugarDeseado) && (pisoDeseado < cantidadPisos)
+                            && (lugarDeseado < cantidadEspacios)) {
+                        if (Sensor.sensores[pisoDeseado][lugarDeseado] != null) {
+                            System.out.println(Sensor.sensores[pisoDeseado][lugarDeseado].toString());
+                        }
+                    } else {
+                        System.out.println("Este lugar no existe");
+                    }
+
+                    break;
+                case 7:
+                    System.out.println(Sensor.sensoresEstado());
+                    break;
+                case 8:
+                    System.out.println("Ingrese el color deseado");
+                    String color_deseado = entrada.next();
+                    System.out.println(Vehiculo.filtroColor(color_deseado));
+                    break;
+                case 9:
+                    for (int i = 0; i < Vehiculo.Ordenar().length; i++) {
+                        System.out.println(Vehiculo.Ordenar()[i].toString());
+                    }
+                    break;
+                default:
+                    System.out.println("Comando incorrecto");
+                    break;
             }
             System.out.println("Escriba una opcion: ");
             System.out.println("0. Salir del sistema");
@@ -140,7 +147,7 @@ public class Principal {
             System.out.println("7. Listar estado de sensores");
             System.out.println("8. Filtrar por color");
             System.out.println("9. Ordenar por valor comercial");
-            seleccion=entrada.nextInt();
+            seleccion = entrada.nextInt();
         }
         System.out.println("Saliendo...");
 
